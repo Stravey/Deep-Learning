a705fc6d87b9e58f1e1ed1b9e67c18afca6f1f89
Stravey <2205487298@qq.com>
2026-01-20 22:08:47 +0800

    update seq2seq

---
+++ MRNN/Seq2Seq.py
@@ -0,0 +1,31 @@
+# 序列到序列学习(Seq2Seq)
+
+import collections
+import math
+import torch
+from torch import nn
+from d2l import torch as d2l
+
+#@save
+class Seq2SeqEncoder(nn.Module):
+    """用于序列到序列学习的循环神经网络编码器"""
+    def __init__(self, vocab_size, embed_size, num_hiddens, num_layers,
+                 dropout = 0, **kwargs):
+        super(Seq2SeqEncoder, self).__init__(**kwargs)
+        # 嵌入层
+        self.embedding = nn.Embedding(vocab_size, embed_size)
+        self.rnn = nn.GRU(embed_size, num_hiddens, num_layers,
+                          dropout = dropout)
+
+    def forward(self, X, *args):
+        # 输出'X'的形状：(batch_size,num_steps,embed_size)
+        X = self.embedding(X)
+        # 循环神经网络中 第一个轴对应时间步
+        X = X.permute(1, 0, 2)
+        # 如果未提及状态，则默认为0
+        output, state = self.rnn(X)
+        # output的形状:(num_steps,batch_size,num_hiddens)
+        # state的形状:(num_layers,batch_size,num_hiddens)
+        return output, state
+
+
