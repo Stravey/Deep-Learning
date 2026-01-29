cd92d227246d18a7c4ad6c73606ef620ae1ac6fb
Stravey <2205487298@qq.com>
2026-01-29 12:56:20 +0800

    update self-attention

--- Attention/Self-Attention.py
+++ Attention/Self-Attention.py
@@ -1,12 +1,48 @@
 import math
 import torch
+from matplotlib import pyplot as plt
 from torch import nn
 from d2l import torch as d2l
 
-num_hiddens, num_heads = 100, 5
-attention = d2l.MultiHeadAttention(num_hiddens, num_heads, 0.5)
+num_hidden, num_heads = 100, 5
+attention = d2l.MultiHeadAttention(num_hidden, num_heads, 0.5)
 batch_size, num_queries, valid_lens = 2, 4, torch.tensor([3, 2])
-X = torch.ones((batch_size, num_queries, num_hiddens))
+X = torch.ones((batch_size, num_queries, num_hidden))
 d2l.check_shape(attention(X, X, X, valid_lens),
-                (batch_size, num_queries, num_hiddens))
+                (batch_size, num_queries, num_hidden))
 
+
+# Positional Encoding
+class PositionalEncoding(nn.Module):
+    """Positional Encoding"""
+    def __init__(self, num_hiddens, dropout, max_len=1000):
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        """Create a long enough P"""
+        self.P = torch.zeros((1, max_len, num_hiddens))
+        X = torch.arange(max_len, dtype=torch.float32).reshape(
+            -1, 1) / torch.pow(10000, torch.arange(
+            0, num_hiddens, 2,dtype=torch.float32) / num_hiddens)
+        self.P[:, :, 0::2] = torch.sin(X)
+        self.P[:, :, 1::2] = torch.cos(X)
+
+    def forward(self, T):
+        T = T + self.P[:, :T.shape[1], :].to(X.device)
+        return self.dropout(T)
+
+
+encoding_dim, num_steps = 32, 60
+pos_encoding = PositionalEncoding(encoding_dim, 0)
+X = pos_encoding(torch.zeros((1, num_steps, encoding_dim)))
+P = pos_encoding.P[:, :X.shape[1], :]
+d2l.plot(torch.arange(num_steps), P[0, :, 6:10].T, xlabel='Row (position)',
+         figsize=(6, 2.5), legend=["Col %d" %d for d in torch.arange(6, 10)])
+# plt.show()
+
+for i in range(8):
+    print(f'{i} in binary is {i :> 03b}')
+
+P = P[0, :, :].unsqueeze(0).unsqueeze(0)
+d2l.show_heatmaps(P, xlabel='Column (encoding dimension)',
+                  ylabel='Row (position)', figsize=(3.5, 4), cmap='Blues')
+plt.show()
